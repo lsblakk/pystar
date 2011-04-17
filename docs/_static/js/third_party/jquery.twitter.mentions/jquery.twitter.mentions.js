276bdc31cd973e4860910c9869df66f6b4f1b73b
Gregg Lind <gregg.lind@gmail.com>
2011-04-17 15:33:03 -0500

    readded trans inclusive language

--- docs/_static/js/third_party/jquery.twitter.mentions/jquery.twitter.mentions.js
+++ docs/_static/js/third_party/jquery.twitter.mentions/jquery.twitter.mentions.js
@@ -41,10 +41,10 @@
     callback: null,
     page : 1,
     maximum : 10,
-	avatar : true,
+    avatar : true,
     ulClass : 'twitter-mentions',
-	odd : true,
-	oddClass : 'odd'
+    odd : true,
+    oddClass : 'odd'
   };
 
   /** private methods **/
@@ -54,11 +54,11 @@
     for ( var i = 0; i < object.length; i++ )
     {
         current = object[i];
-		user = current.from_user;
-		text = current.text.replace(/(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/, function (u) {
+        user = current.from_user;
+        text = current.text.replace(/(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/, function (u) {
             var shortUrl = (u.length > 30) ? u.substr(0, 30) + '...': u;
             return '<a href="' + u + '">' + shortUrl + '</a>';
-		}).replace(/@([a-zA-Z0-9_]+)/g, '@<a href="http://twitter.com/$1">$1</a>').replace(/(?:^|\s)#([^\s\.\+:!]+)/g, function (a, u) {
+        }).replace(/@([a-zA-Z0-9_]+)/g, '<a href="http://twitter.com/$1">$1</a>').replace(/(?:^|\s)#([^\s\.\+:!]+)/g, function (a, u) {
             return ' <a href="http://twitter.com/search?q=' + encodeURIComponent(u) + '">#' + u + '</a>';
         });
         html += '<li'+( (opts.odd && i%2 ) ? ' class="'+opts.oddClass+'"' : '')+'>'+( opts.avatar ? '<a href="http://twitter.com/'+user+'"><img src="'+ current.profile_image_url +'"></a>' : '')+'<strong>'+ user + '</strong> '+ text +'</li>' ;
@@ -70,9 +70,9 @@
   var _query = function(q)
   {
     if( typeof q == 'string' )
-        return '@'+q;
+        return q
     else if( typeof q == 'object' )
-        return '@'+q.join(' OR @');
+        return q.join(' OR ');
     throw 'data type not supported';
   }
 })(jQuery);
